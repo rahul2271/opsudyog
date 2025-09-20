06740c523c17188f775a56e44dd5a0c34f2933fd
Rahul2271 <rraahhuullch@gmail.com>
2025-09-20 16:30:55 +0530

    s

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/products/:path*", 
-        destination: "https://darkcyan-gnu-869252.hostingersite.com/products/:path*", 
+        source: "/products/:path*",
+        destination: "https://darkcyan-gnu-869252.hostingersite.com/products/:path*",
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
