0a50de53b78040679ab4f3ce17bdebda24ac9a05
Rahul2271 <rraahhuullch@gmail.com>
2025-09-20 17:18:56 +0530

    S

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // user visits yourdomain.com/products/some-slug
-        source: "/products/:slug*", 
-        // proxy to WordPress (note: singular /product/)
-        destination: "https://darkcyan-gnu-869252.hostingersite.com/product/:slug*", 
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
